a04c1cb5a42608c4dfd9ac6e2e755b85d164e167
amsantac <amsantac@gmail.com>
2014-10-29 17:19:12 -0400

    upload modified crosstab algorithm

--- R/TOC.R
+++ R/TOC.R
@@ -1,6 +1,7 @@
 TOC <- function(input, ref, mask=NULL, nthres, NAvalue=0) {
 
-if(is.null(mask)) mask <- ref-ref+1
+if(is.null(mask)) 
+    mask <- ref - ref + 1
 mask[mask == NAvalue] <- NA
 
 input <- input*mask
@@ -12,30 +13,39 @@ inpval <- getValues(input)
 srted.ref <- sort(refval, index.return=TRUE, na.last=NA)
 srted.inp <- sort(inpval, index.return=TRUE, na.last=NA)
 
-maxInp <- max(srted.inp$ix)
-newThres <- ceiling(maxInp/nthres*seq(1:nthres))
-newThres <- sort(newThres, decreasing=TRUE)
-srted.inp$ordInp <- 1:maxInp
+maxRank <- max(srted.inp$ix)
+srted.inp$inputRank <- 1:maxRank
 
 mrg.ir <- merge(srted.ref, srted.inp, by.x="ix", by.y="ix")
 mrg.ir$thresB <- 0
+
+newThres <- ceiling(maxRank/nthres*seq(1:nthres))
+newThres <- sort(newThres, decreasing=TRUE)
 
 ones.ref <- table(refval)[[2]]
 zeros.ref <- table(refval)[[1]]
 
 res <- cbind(newThres, "truePositive"=0, "truePosRate"=0, "falsePositive"=0, "falsePosRate"=0)
 
+func_logical3   <- function(v1,v2){
+    r1  <- sum(v1 & v2)
+    r2  <- sum(v1 & !v2)
+    return(c(r1, r2))
+}
+
 for (j in 2:(nrow(res))){
 i <- newThres[j]
-mrg.ir[which(mrg.ir$ordInp > i), "thresB"]  <- 1
+mrg.ir[which(mrg.ir$inputRank > i), "thresB"]  <- 1
 
-crsstb <- table(mrg.ir$thresB, mrg.ir$x.x)
+xb <- as.bit(mrg.ir$thresB)
+yb <- as.bit(mrg.ir$x.x)
+crsstb <- func_logical3(xb,yb)
+
+res[j,"truePositive"] <- crsstb[1]
+res[j,"truePosRate"] <- crsstb[1]/ones.ref*100
+res[j,"falsePositive"] <- crsstb[2]
+res[j,"falsePosRate"] <- crsstb[2]/zeros.ref*100
 mrg.ir$thresB <- 0
-res[j,"truePositive"] <- crsstb[2,2]
-res[j,"truePosRate"] <- crsstb[2,2]/ones.ref*100
-res[j,"falsePositive"] <- crsstb[2,1]
-res[j,"falsePosRate"] <- crsstb[2,1]/zeros.ref*100
-
 }
 
 res <- as.data.frame(rbind(res, c(0, ones.ref, 100, zeros.ref, 100)))
