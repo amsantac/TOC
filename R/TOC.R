8b8ab6b374b73ce1673c274632fe4ebf9482b02f
amsantac <amsantac@gmail.com>
2014-12-26 12:30:07 -0500

    new plot function and updated TOC function

--- R/TOC.R
+++ R/TOC.R
@@ -1,31 +1,20 @@
-TOC <- function(input, ref, mask=NULL, nthres, NAvalue=0) {
+TOC <- function(index, boolean, mask=NULL, nthres=NULL, thres=NULL, NAval=0, ranking=FALSE, 
+P=NA, Q=NA, uncertainty=FALSE) {
+
+if(!is.null(nthres) & !is.null(thres)) stop("Enter nthres OR thres as input, not both at the same time")
 
 if(is.null(mask)) 
-    mask <- ref - ref + 1
-mask[mask == NAvalue] <- NA
+    mask <- boolean*0 + 1
+mask[mask == NAval] <- NA
 
-input <- input*mask
-ref <- ref*mask
+index <- index*mask
+boolean <- boolean*mask
 
-refval <- getValues(ref)
-inpval <- getValues(input)
+boolval <- getValues(boolean)
+indval <- getValues(index)
 
-srted.ref <- sort(refval, index.return=TRUE, na.last=NA)
-srted.inp <- sort(inpval, index.return=TRUE, na.last=NA)
-
-maxRank <- max(srted.inp$ix)
-srted.inp$inputRank <- 1:maxRank
-
-mrg.ir <- merge(srted.ref, srted.inp, by.x="ix", by.y="ix")
-mrg.ir$thresB <- 0
-
-newThres <- ceiling(maxRank/nthres*seq(1:nthres))
-newThres <- sort(newThres, decreasing=TRUE)
-
-ones.ref <- table(refval)[[2]]
-zeros.ref <- table(refval)[[1]]
-
-res <- cbind(newThres, "truePositive"=0, "truePosRate"=0, "falsePositive"=0, "falsePosRate"=0)
+ones.bool <- table(boolval)[[2]]
+zeros.bool <- table(boolval)[[1]]
 
 func_logical3   <- function(v1,v2){
     r1  <- sum(v1 & v2)
@@ -33,21 +22,142 @@ func_logical3   <- function(v1,v2){
     return(c(r1, r2))
 }
 
+if(!ranking){
+boolval <- boolval[!is.na(boolval)]
+indval <- indval[!is.na(indval)]
+
+if (length(boolval)!=length(indval)) stop('different NA values in input maps')
+
+zeroIndVal <- indval*0
+maxInd <- max(indval, na.rm=TRUE)
+
+ifelse(!is.null(thres), minInd <- min(thres), minInd <- min(indval, na.rm=TRUE))
+
+ifelse(!is.null(nthres), newThres <- (maxInd - minInd)/(nthres-2)*(0:(nthres-2)) + minInd, ifelse(!is.null(thres), newThres <- thres, newThres <- unique(indval)))
+newThres <- sort(newThres, decreasing=TRUE)
+
+res <- cbind(newThres, "Hits"=0, "HitsRate"=0, "falseAlarms"=0, "falseAlarmsRate"=0)
+
 for (j in 2:(nrow(res))){
 i <- newThres[j]
-mrg.ir[which(mrg.ir$inputRank > i), "thresB"]  <- 1
+zeroIndVal[which(indval > i)]  <- 1
+
+xb <- as.bit(zeroIndVal)
+yb <- as.bit(boolval)
+crsstb <- func_logical3(xb,yb)
+
+res[j,"Hits"] <- crsstb[1]
+res[j,"HitsRate"] <- crsstb[1]/ones.bool*100
+res[j,"falseAlarms"] <- crsstb[2]
+res[j,"falseAlarmsRate"] <- crsstb[2]/zeros.bool*100
+zeroIndVal <- indval*0
+}
+}
+
+if(ranking){
+srted.bool <- sort(boolval, index.return=TRUE, na.last=NA)
+srted.ind <- sort(indval, index.return=TRUE, na.last=NA)
+
+maxRank <- max(srted.ind$ix)
+minRank <- min(srted.ind$ix)
+srted.ind$indexRank <- 1:maxRank
+
+mrg.ir <- merge(srted.bool, srted.ind, by.x="ix", by.y="ix")
+mrg.ir$thresB <- 0
+
+indval <- indval[!is.na(indval)]
+minInd <- min(indval, na.rm=TRUE)
+
+newThres <- maxRank/nthres*(1:nthres)
+newThres <- sort(newThres, decreasing=TRUE)
+
+res <- cbind(newThres, "Hits"=0, "HitsRate"=0, "falseAlarms"=0, "falseAlarmsRate"=0)
+
+for (j in 2:(nrow(res))){
+i <- newThres[j]
+mrg.ir[which(mrg.ir$indexRank > i), "thresB"]  <- 1
 
 xb <- as.bit(mrg.ir$thresB)
 yb <- as.bit(mrg.ir$x.x)
 crsstb <- func_logical3(xb,yb)
 
-res[j,"truePositive"] <- crsstb[1]
-res[j,"truePosRate"] <- crsstb[1]/ones.ref*100
-res[j,"falsePositive"] <- crsstb[2]
-res[j,"falsePosRate"] <- crsstb[2]/zeros.ref*100
+res[j,"Hits"] <- crsstb[1]
+res[j,"HitsRate"] <- crsstb[1]/ones.bool*100
+res[j,"falseAlarms"] <- crsstb[2]
+res[j,"falseAlarmsRate"] <- crsstb[2]/zeros.bool*100
 mrg.ir$thresB <- 0
 }
+}
 
-res <- as.data.frame(rbind(res, c(0, ones.ref, 100, zeros.ref, 100)))
-return(res)
+tocd <- as.data.frame(rbind(res, c(NA, ones.bool, 100, zeros.bool, 100)))
+#res <- rbind(res, c(0, ones.bool, 100, zeros.bool, 100))
+#return(as.data.frame(res))
+
+names(tocd) <- c("Threshold", "A", "HitsRate", "B", "falseAlarmsRate")
+
+validPixels <- ones.bool + zeros.bool
+
+population <- validPixels * res(index)[1] * res(index)[2]
+if(!is.na(P) & !is.na(Q)){
+population <- P + Q
+}
+
+tocd$Model1 <- tocd$HitsRate/100
+tocd$falseAlarms1 <- tocd$falseAlarmsRate/100
+tocd$Uniform <- tocd$falseAlarms1
+maxA <- tocd$A[nrow(tocd)]
+maxB <- tocd$B[nrow(tocd)]
+tocd$m <- (maxA - tocd$A)/(maxA + maxB) 
+tocd$h <- tocd$A/(maxA + maxB)
+tocd$f <- tocd$B/(maxA + maxB) 
+tocd$c <- 1 - tocd$m - tocd$h -tocd$f
+prevalence <- tocd$h[nrow(tocd)]
+
+tocd$Hits <- tocd$Model1 * prevalence * population
+tocd$hitsFalseAlarms <- tocd$Hits + tocd$falseAlarms1*(1-prevalence)*population
+tocd$hitsMisses <- prevalence*population
+tocd$maximum <- pmin(tocd$hitsMisses, tocd$hitsFalseAlarms)
+tocd$minimum <- pmax(0, tocd$hitsFalseAlarms + tocd$hitsMisses - population)
+tocd$Uniform1 <- tocd$hitsMisses * tocd$hitsFalseAlarms / population
+
+tocd1 <- tocd
+tocd1[nrow(tocd1), "Threshold"] <- paste("<= ", minInd)
+
+tocd2 <- tocd1[, c("Threshold", "hitsFalseAlarms", "Hits")]
+
+if(!is.na(P) & !is.na(Q)){
+tocd1$hitsFalseAlarmsP <- P * tocd1$Model1 + Q * tocd1$falseAlarms1
+tocd1$hitsP <- P * tocd1$Model1
+tocd2 <- tocd1[, c("Threshold", "hitsFalseAlarms", "Hits", "hitsFalseAlarmsP", "hitsP")]
+}
+
+units <- strsplit(strsplit(CRSargs(crs(index)), "+units=")[[1]][2], " ")[[1]][1]
+
+id <- order(tocd2$hitsFalseAlarms)
+totalAUC <- sum(tocd2$Hits[-length(tocd2$Hits)] * diff(tocd2$hitsFalseAlarms)) + sum(diff(tocd2$hitsFalseAlarms[id])*diff(tocd2$Hits[id]))/2 - ((prevalence * population)^2)/2 
+AUC <- totalAUC/(population * prevalence * population - (prevalence * population)^2)
+
+if(!uncertainty) return(list(TOCtable=tocd2, AUC=AUC, units=units, prevalence=prevalence*population, population=population))
+else
+{
+thist <- hist(unique(index), breaks=c(sort(tocd$Threshold)), plot=FALSE)
+tocd$counts <- c(0, thist$counts[length(thist$counts):1], 0)
+
+uncertain <- 0
+for (i in 2:(nrow(tocd)-2)){
+if(tocd[i,"counts"] > 1) {
+area <- (tocd[i,"falseAlarms1"] - tocd[i-1,"falseAlarms1"])*(tocd[i,"Model1"] - tocd[i-1,"Model1"])
+uncertain <- uncertain + area
+}
+}
+
+i <- i+1
+if(tocd[i,"counts"] > 2) {
+area <- (tocd[i,"falseAlarms1"] - tocd[i-1,"falseAlarms1"])*(tocd[i,"Model1"] - tocd[i-1,"Model1"])
+uncertain <- uncertain + area
+}
+
+return(list(TOCtable=tocd2, AUC=AUC, units=units, prevalence=prevalence*population, population=population, maxAUC = AUC + uncertain/2, minAUC = AUC - uncertain/2))
+}
+
 }
---
+++ R/plot.TOC.R
@@ -0,0 +1,52 @@
+plot.TOC <- function(toc, labelThres=FALSE, digits=3){
+
+population <- toc$population
+prevalence <- toc$prevalence/population
+units <- toc$units
+
+tocd <- toc$TOCtable
+if(!is.null(tocd$HitsP) & !is.null(tocd$hitsFalseAlarmsP)){
+tocd$Hits <- tocd$HitsP
+tocd$hitsFalseAlarms <- tocd$hitsFalseAlarmsP
+}
+
+
+#units <- strsplit(strsplit(CRSargs(crs(index)), "+units=")[[1]][2], " ")[[1]][1]
+old.par <- par(no.readonly = TRUE)
+par(oma = c(0, 0, 0, 4))
+par(mgp = c(1.5, 1, 0))
+
+plot(c(0, population*(1-prevalence), population), c(0, 0, prevalence * population), type="l", lty="dashed", xlab=paste("Hits+False Alarms (square ", units, ")", sep=""), ylab=paste("Hits (square ", units, ")", sep=""), lwd=2, col=rgb(128,100,162, maxColorValue=255), bty="n", xaxt="n", yaxt="n", xlim=c(0, 1.05*population), ylim=c(0, 1.05*prevalence * population), asp=1/prevalence)
+
+axis(1, pos=0)
+axis(2, pos=0)
+
+# maximum
+lines(c(0, prevalence * population, population), c(0, prevalence * population, prevalence * population), lty="dotdash", lwd=2, col=rgb(79,129,189, maxColorValue=255)) 
+
+# hits+misses
+lines(c(0, population), rep(prevalence*population, 2), lwd=3, col=rgb(146,208,80, maxColorValue=255))
+
+# uniform
+lines(c(0, population), c(0, prevalence*population), lty="dotted", lwd=2, col=rgb(0,0,255, maxColorValue=255))
+
+#lines(tocd$hitsFalseAlarms, tocd$maximum, lty="dotdash", lwd=2, col=rgb(79,129,189, maxColorValue=255))
+
+# model
+lines(tocd$hitsFalseAlarms, tocd$Hits, lwd=2, col=rgb(255,0,0, maxColorValue=255))
+points(tocd$hitsFalseAlarms, tocd$Hits, pch=17, col=rgb(255,0,0, maxColorValue=255))
+if(labelThres == TRUE) text(tocd$hitsFalseAlarms, tocd$Hits, round(as.numeric(tocd$Threshold), digits))
+
+par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
+plot(0, 0, type = "n", bty = "n", xaxt = "n", yaxt = "n")
+
+legend("right", c("Hits+Misses", "Maximum", "Model", "Uniform", "Minimum"), col = c(rgb(146,208,80, maxColorValue=255), rgb(79,129,189, maxColorValue=255), rgb(255,0,0, maxColorValue=255), rgb(0,0,255, maxColorValue=255), rgb(128,100,162, maxColorValue=255)), lty = c(1, 4, 1, 3, 2), pch = c(NA, NA, 17, NA, NA),
+       merge = TRUE, bty="n", lwd=c(3, 2, 2, 2, 2))
+par(old.par)
+
+}
+
+
+#setMethod("plot", signature("TOC"), plot.TOC)
+
+#}
