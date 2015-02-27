0d31e45b44b0614b7ad158ae6d8efcbf4a89aa91
amsantac <amsantac@gmail.com>
2015-02-27 15:51:09 -0500

    include new ROC and plot.ROC functions

--- R/ROC.R
+++ R/ROC.R
@@ -1,4 +1,4 @@
-TOC <- function(index, boolean, mask=NULL, nthres=NULL, thres=NULL, NAval=0, ranking=FALSE, 
+ROC <- function(index, boolean, mask=NULL, nthres=NULL, thres=NULL, NAval=0, ranking=FALSE, 
 P=NA, Q=NA, uncertainty=TRUE, progress=FALSE) {
 
 if(!is.null(nthres) & !is.null(thres)) stop("Enter nthres OR thres as input, not both at the same time")
@@ -40,21 +40,22 @@ indval <- indval[!is.na(indval)]
 
 if (length(boolval)!=length(indval)) stop('different NA values in input maps')
 
+
 zeroIndVal <- indval*0
 maxInd <- max(indval, na.rm=TRUE)
 
-# define the thresholds vector
 ifelse(!is.null(thres), minInd <- min(thres), minInd <- min(indval, na.rm=TRUE))
 
 ifelse(!is.null(nthres), newThres <- (maxInd - minInd)/(nthres-2)*(0:(nthres-2)) + minInd, ifelse(!is.null(thres), 
                                 newThres <- thres, newThres <- unique(indval)))
 newThres <- sort(newThres, decreasing=TRUE)
 
-# create results data.frame
 res <- cbind(newThres, "Hits"=0, "HitsRate"=0, "falseAlarms"=0, "falseAlarmsRate"=0)
 
 # loop for reclassifying the index vector for each new threshold
 # and then perform the crosstab with the boolean vector
+
+
 
 for (j in 2:(nrow(res))){
 i <- newThres[j]
@@ -119,10 +120,11 @@ if(progress){
 }
 
 tocd <- as.data.frame(rbind(res, c(NA, ones.bool, 100, zeros.bool, 100)))
+#res <- rbind(res, c(0, ones.bool, 100, zeros.bool, 100))
+#return(as.data.frame(res))
 
 names(tocd) <- c("Threshold", "A", "HitsRate", "B", "falseAlarmsRate")
 
-# calculate population 
 validPixels <- ones.bool + zeros.bool
 
 population <- validPixels * res(index)[1] * res(index)[2]
@@ -130,52 +132,47 @@ if(!is.na(P) & !is.na(Q)){
 population <- P + Q
 }
 
-# calculate TOC table as shown in TOCfigure1.xlsx created by Pontius
 tocd$Model1 <- tocd$HitsRate/100
 tocd$falseAlarms1 <- tocd$falseAlarmsRate/100
 tocd$Uniform <- tocd$falseAlarms1
-maxA <- tocd$A[nrow(tocd)]
-maxB <- tocd$B[nrow(tocd)]
-tocd$m <- (maxA - tocd$A)/(maxA + maxB) 
-tocd$h <- tocd$A/(maxA + maxB)
-tocd$f <- tocd$B/(maxA + maxB) 
-tocd$c <- 1 - tocd$m - tocd$h -tocd$f
-prevalence <- tocd$h[nrow(tocd)]
 
-tocd$Hits <- tocd$Model1 * prevalence * population
-tocd$hitsFalseAlarms <- tocd$Hits + tocd$falseAlarms1*(1-prevalence)*population
-tocd$hitsMisses <- prevalence*population
-tocd$maximum <- pmin(tocd$hitsMisses, tocd$hitsFalseAlarms)
-tocd$minimum <- pmax(0, tocd$hitsFalseAlarms + tocd$hitsMisses - population)
-tocd$Uniform1 <- tocd$hitsMisses * tocd$hitsFalseAlarms / population
 
-tocd1 <- tocd
-tocd1[nrow(tocd1), "Threshold"] <- paste("<= ", minInd)
-
-tocd2 <- tocd1[, c("Threshold", "hitsFalseAlarms", "Hits")]
-
-# adjustment to population if user provides P and Q
-if(!is.na(P) & !is.na(Q)){
-tocd1$hitsFalseAlarmsP <- P * tocd1$Model1 + Q * tocd1$falseAlarms1
-tocd1$HitsP <- P * tocd1$Model1
-tocd2 <- tocd1[, c("Threshold", "hitsFalseAlarms", "Hits", "hitsFalseAlarmsP", "HitsP")]
-}
-
-# extract map units for plotting purposes with plot.TOC
+# maxA <- tocd$A[nrow(tocd)]
+# maxB <- tocd$B[nrow(tocd)]
+# tocd$m <- (maxA - tocd$A)/(maxA + maxB) 
+# tocd$h <- tocd$A/(maxA + maxB)
+# tocd$f <- tocd$B/(maxA + maxB) 
+# tocd$c <- 1 - tocd$m - tocd$h -tocd$f
+# prevalence <- tocd$h[nrow(tocd)]
+# 
+# tocd$Hits <- tocd$Model1 * prevalence * population
+# tocd$hitsFalseAlarms <- tocd$Hits + tocd$falseAlarms1*(1-prevalence)*population
+# tocd$hitsMisses <- prevalence*population
+# tocd$maximum <- pmin(tocd$hitsMisses, tocd$hitsFalseAlarms)
+# tocd$minimum <- pmax(0, tocd$hitsFalseAlarms + tocd$hitsMisses - population)
+# tocd$Uniform1 <- tocd$hitsMisses * tocd$hitsFalseAlarms / population
+# 
+# tocd1 <- tocd
+# tocd1[nrow(tocd1), "Threshold"] <- paste("<= ", minInd)
+# 
+# tocd2 <- tocd1[, c("Threshold", "hitsFalseAlarms", "Hits")]
+# 
+# if(!is.na(P) & !is.na(Q)){
+# tocd1$hitsFalseAlarmsP <- P * tocd1$Model1 + Q * tocd1$falseAlarms1
+# tocd1$HitsP <- P * tocd1$Model1
+# tocd2 <- tocd1[, c("Threshold", "hitsFalseAlarms", "Hits", "hitsFalseAlarmsP", "HitsP")]
+# }
+# 
 units <- strsplit(strsplit(CRSargs(crs(index)), "+units=")[[1]][2], " ")[[1]][1]
 
-# calculate totalAUC in squared map units and AUC as a proportion 
-id <- order(tocd2$hitsFalseAlarms)
-totalAUC <- sum(tocd2$Hits[-length(tocd2$Hits)] * diff(tocd2$hitsFalseAlarms)) + sum(diff(tocd2$hitsFalseAlarms[id])*diff(tocd2$Hits[id]))/2 - ((prevalence * population)^2)/2 
-AUC <- totalAUC/(population * prevalence * population - (prevalence * population)^2)
+id <- order(tocd$falseAlarms1)
+AUC <- sum(tocd$Model1[-length(tocd$Model1)] * diff(tocd$falseAlarms1)) + sum(diff(tocd$falseAlarms1[id])*diff(tocd$Model1[id]))/2 
+#AUC <- totalAUC/(population * prevalence * population - (prevalence * population)^2)
+# 
+# colnames(tocd2)[2] <- "Hits+FalseAlarms"
+# if (any(colnames(tocd2) == "hitsFalseAlarmsP")) colnames(tocd2)[4] <- "Hits+FalseAlarmsP"
 
-colnames(tocd2)[2] <- "Hits+FalseAlarms"
-if (any(colnames(tocd2) == "hitsFalseAlarmsP")) colnames(tocd2)[4] <- "Hits+FalseAlarmsP"
-
-# if uncertainty calculation is not requested by the user
-if(!uncertainty) return(list(TOCtable=tocd2, AUC=AUC, units=units, prevalence=prevalence*population, population=population))
-
-# if uncertainty calculation is requested by the user
+if(!uncertainty) return(list(ROCtable=tocd, AUC=AUC, units=units))
 else
 {
 thist <- hist(unique(index), breaks=c(sort(tocd$Threshold)), plot=FALSE)
@@ -195,7 +192,7 @@ area <- (tocd[i,"falseAlarms1"] - tocd[i-1,"falseAlarms1"])*(tocd[i,"Model1"] - 
 uncertain <- uncertain + area
 }
 
-return(list(TOCtable=tocd2, AUC=AUC, units=units, prevalence=prevalence*population, population=population, maxAUC = AUC + uncertain/2, minAUC = AUC - uncertain/2))
+return(list(ROCtable=tocd[, -ncol(tocd)], AUC=AUC, units=units, maxAUC = AUC + uncertain/2, minAUC = AUC - uncertain/2))
 }
 
 }
